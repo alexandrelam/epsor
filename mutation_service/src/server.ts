1f45299a932bf38ec1a2f98cf262aafe39581c33
Alexandre Lam <alexandrelam@outlook.com>
2022-01-16 18:58:29 +0100

    refactor: moved db connection

--- mutation_service/src/server.ts
+++ mutation_service/src/server.ts
@@ -1,7 +1,9 @@
 import createBook from "./operations/create";
 import deleteBook from "./operations/delete";
+import mongoose from "mongoose";
 
 const startServer = async () => {
+  mongoose.connect("mongodb://mongo:27017/book");
   createBook();
   deleteBook();
 };
